f23ed3f4c5bee44bb4c5e7c625b9e454b41ecd27
Jim Bacon <jame2@ceh.ac.uk>
2021-05-14 22:43:47 +0100

    Try enabling automatic section labels

--- conf.py
+++ conf.py
@@ -27,7 +27,14 @@ todo_include_todos=True
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.ifconfig']
+extensions = [
+  'sphinx.ext.todo', 
+  'sphinx.ext.ifconfig', 
+  'sphinx.ext.autosectionlabel'
+]
+
+# Make sure the autosection target is unique
+autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
